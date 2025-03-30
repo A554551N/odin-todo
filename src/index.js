ae00d036eb3dd1509462ce9da3f86c7c91b0b213
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-30 12:02:10 -0400

    implement basics of ToDoItem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./styles.css"
+import ToDoItem from "./todo-item.js"
 
 console.log("Loaded")
+
+const testItem = new ToDoItem("Test","A Test Item",new Date(),3);
+console.table(testItem);
+console.log(`Priority: ${testItem.priority}`)
+testItem.priority = "HIGH"
+console.log(`Priority: ${testItem.priority}`)
---
+++ src/todo-item.js
@@ -0,0 +1,42 @@
+export default class ToDoItem {
+    constructor(title,description="",dueDate,priorityValue) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        // date is going to be a bear, always is.  For now I'll take the date from the 
+        // form as it comes, but this will likely eat up a lot of my time later. 
+        this.priorityValue = priorityValue
+        // I'm choosing to store the priority as a 3 step value (1 is normal,
+        // 2 is high, 3 is critical)
+        // I'll probably have a get function further down that returns a text expression for
+        // use in other elements of the app.
+    }
+
+    get priority() {
+        const priorityLevels = {
+            1: "Normal",
+            2: "High",
+            3: "Critical"
+        }
+        return priorityLevels[this.priorityValue]
+    }
+
+    set priority(priorityString) {
+        const sanitizedPriorityString = priorityString.toLowerCase();
+        const priorityLevels = {
+            "normal": 1,
+            "high": 2,
+            "critical": 3
+        }
+        this.priorityValue = priorityLevels[sanitizedPriorityString];
+    }
+}
+/*
+I'm going to include comment notes on this module while I build it, and then remove them at the end.
+This is the basic item, a ToDoItem.  It holds all of the data of a single to-do element.
+
+Currently, properties can be set directly by other elements, but my suspicion is that I'll need
+a set method on the date at some point.
+
+I want to include as little logic on this class as possible because I would prefer if these could be directly serialized.z
+*/
