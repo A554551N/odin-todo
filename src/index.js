faaf78692b7f2a9646a4a6a9fda4c91ebd991571
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-30 18:35:00 -0400

    implement serialize() on ItemGroup for save/load

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import ToDoItem from "./todo-item.js"
 import ItemGroup from "./item-group.js";
 
 const itemGroup = new ItemGroup("Test Group","Some Sample Elements");
-
+const masterGroup = new ItemGroup("Master Group","");
 const testItem = new ToDoItem("Test","A Test Item",new Date(),3);
 const testTwo = new ToDoItem("Test 2","A Second Test",new Date(),1);
 
 itemGroup.addToGroup(testItem);
 itemGroup.addToGroup(testTwo);
 
-console.table(itemGroup);
+//console.table(itemGroup);
 
 const returnedItem = itemGroup.getFromGroup(testItem.id);
 
 if (returnedItem === testItem) {
-    console.log("Returned True");
+    //console.log("Returned True");
 }
 
 returnedItem.title = "Updated Title";
 itemGroup.updateGroup(returnedItem);
+masterGroup.addToGroup(itemGroup);
+//console.table(itemGroup);
+//console.table(itemGroup);
 
-console.table(itemGroup);
-
-itemGroup.deleteFromGroup(testTwo.id);
-console.table(itemGroup);
+console.log(masterGroup.serialize());
--- src/item-group.js
+++ src/item-group.js
@@ -10,13 +10,13 @@ export default class ItemGroup {
         return this._id;
     }
 
-    addToGroup(toDoItem) {
-        this.contents.push(toDoItem);
+    addToGroup(itemToAdd) {
+        this.contents.push(itemToAdd);
     }
 
-    deleteFromGroup(toDoItemID) {
+    deleteFromGroup(itemID) {
         for (let i = 0; i < this.contents.length; i++) {
-            if (this.contents[i].id === toDoItemID) {
+            if (this.contents[i].id === itemID) {
                 this.contents.splice(i,1);
                 return true;
             }
@@ -24,16 +24,37 @@ export default class ItemGroup {
         }
     }
 
-    getFromGroup(toDoItemID) {
+    getFromGroup(itemID) {
         for (let i = 0; i < this.contents.length; i++) {
-            if (this.contents[i].id === toDoItemID) {
+            if (this.contents[i].id === itemID) {
                 return this.contents[i];
             }
         }
     }
 
-    updateGroup(toDoItem) {
-        this.deleteFromGroup(toDoItem.id);
-        this.addToGroup(toDoItem);
+    updateGroup(itemToUpdate) {
+        this.deleteFromGroup(itemToUpdate.id);
+        this.addToGroup(itemToUpdate);
+    }
+
+    serialize() {
+        const mainGroupArr = [];
+        for (const item of this.contents) {
+            if (item instanceof ItemGroup) {
+                console.log("entered IF")
+                const contentsArr = new Array();
+                for (const subItem of item.contents) {
+                    contentsArr.push(subItem);
+                }
+                const subItemOutput = {id:item.id, name:item.name, description:item.description, contents:contentsArr}
+                console.log(subItemOutput);
+                mainGroupArr.push(JSON.stringify(subItemOutput));
+            } else {
+                mainGroupArr.push(JSON.stringify(item));
+            }
+        }
+        console.log(`mainGroupArr ${mainGroupArr}`);
+        const jsonOutput = {id:this.id, name:this.name, description:this.description, contents:mainGroupArr};
+        return JSON.stringify(jsonOutput);
     }
 }
