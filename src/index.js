884926003fe02506058a2074218a300aa602e477
Justin Gordon <linuxfreak1030@gmail.com>
2025-04-06 11:03:23 -0400

    add new-list-modal div to hold new list interface

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import DataHandler from "./dataHandler.js";
 const dataHandler = new DataHandler();
 const domManager = new DOMManager(dataHandler);
 
-dataHandler.createNewItem("Test Item",new Date());
+dataHandler.createNewItem("Test Item",new Date(),"Some description text to describe the task.");
 dataHandler.createNewItem("Test Item",new Date());
 dataHandler.createNewItem("Test Item",new Date());
 const newGroupID = dataHandler.createNewGroup("Second Group");
