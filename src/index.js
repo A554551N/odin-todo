e90b5859aa8ba634959fd25486d3a0b8a75fc7b9
Justin Gordon <linuxfreak1030@gmail.com>
2025-04-19 19:24:17 -0400

    update test values

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import DataHandler from "./dataHandler.js";
 const dataHandler = new DataHandler();
 const domManager = new DOMManager(dataHandler);
 
-dataHandler.createNewItem("Test Item",new Date(),"Some description text to describe the task.");
-dataHandler.createNewItem("Test Item",new Date());
-dataHandler.createNewItem("Test Item",new Date());
+dataHandler.createNewItem("Test Item 1",new Date(),"Some description text to describe the task.");
+dataHandler.createNewItem("Test Item 2",new Date());
+dataHandler.createNewItem("Test Item 3",new Date());
 const newGroupID = dataHandler.createNewGroup("Second Group");
 dataHandler.activeGroup = newGroupID;
-dataHandler.createNewItem("2nd Group",new Date());
-dataHandler.createNewItem("2nd Group 2",new Date());
+dataHandler.createNewItem("2nd Group Item",new Date(),"A short description for test");
+dataHandler.createNewItem("2nd Group Item 2",new Date());
 domManager.updateMainContent();
 domManager.updateSidebar();
